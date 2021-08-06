eda1f12ed94e8804a1c59f7158e07dc3d8970b0f
Tycho <info@bespokeweb.nl>
2021-08-06 13:58:23 +0200

    return weird follow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'js-expansion'
-import Fetcher from './Fetcher'
-
 export {default as Router} from './Router'
+import Fetcher from './Fetcher'
 export {default as Client} from './Client'
 export {default as Request} from './Request'
 
