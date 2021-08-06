9a227ace36b9adea64a0a385c1732fe9bbbdddeb
Tycho <info@bespokeweb.nl>
2021-08-06 14:23:34 +0200

    remove everything

--- src/Client.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const Client = axios.create({
-    withCredentials: true
-})
-
-export default Client
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'js-expansion'
-export {default as Router} from './Router'
-import Fetcher from './Fetcher'
-export {default as Client} from './Client'
-export {default as Request} from './Request'
-
-export default Fetcher
